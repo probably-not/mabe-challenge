d39709e7e620d65ddbac40ed1fd57d8d5dc8a587
Coby Benveniste <coby@base.la>
2022-05-17 09:20:25 +0300

    Comments to make sure I understand

--- index.js
+++ index.js
@@ -16,37 +16,49 @@ const cardsData = fs.readFileSync("./cards.json");
 const cards = JSON.parse(cardsData);
 
 async function getMissingCard(key) {
+  // Get the cards that the user has already seen
   const userCards = await client.zRange(key, 0, -1);
   let allCards = [...cards];
 
+  // Remove each card that the user has already seen
   userCards.forEach((userCard, idx) => {
     allCards = allCards.filter(function (value, index, arr) {
       return JSON.parse(userCard).id !== value.id;
     });
   });
 
+  // Return one of the cards left
   return allCards.pop();
 }
 
 app.get("/card_add", async (req, res) => {
   const key = "user_id:" + req.query.id;
   let missingCard = "";
+
   while (true) {
     missingCard = await getMissingCard(key);
+
+    // ALL CARDS is sent when all cards have been given to the user
     if (missingCard === undefined) {
       res.send({ id: "ALL CARDS" });
       return;
     }
+
+    // Try to acquire the card so we can send it
     result = await client.ZADD(
       key,
       { score: 0, value: JSON.stringify(missingCard) },
       "NX"
     );
+
+    // If we couldn't acquire it, then someone else has already shown that use this card, so we try another card.
     if (result === 0) {
       continue;
     }
+
     break;
   }
+
   res.send(missingCard);
 });
 
