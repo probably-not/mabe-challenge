2a329fd80cf84be8dd64be6280a6b30124ebd79d
Coby Benveniste <coby@base.la>
2022-05-17 13:35:40 +0300

    Revert "replace express with polkadot"
    This reverts commit 0733a5906ab22ed945be8bc1300eeed506d84885.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const fs = require("fs");
-const polkadot = require("polkadot");
+const express = require("express");
+const app = express();
 const port = +process.argv[2] || 3000;
+
+const client = require("redis").createClient();
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+client.on("ready", () => {
+  app.listen(port, "0.0.0.0", () => {
+    console.log(`Example app listening at http://0.0.0.0:${port}`);
+  });
+});
 
 const cardsData = fs.readFileSync("./cards.json");
 const cards = JSON.parse(cardsData);
@@ -46,37 +56,22 @@ async function getMissingCard(key) {
   return unseenCard;
 }
 
-const app = polkadot(async (req, res) => {
-  if (req.path === "/ready") {
-    res.statusCode = 200;
-    res.end(JSON.stringify({ ready: true }));
+app.get("/card_add", async (req, res) => {
+  initializeAllCards(); // Needs to run when requests are live so that it doesn't get flushed by the tester
+  const key = "user_id:" + req.query.id;
+  unseenCard = await getMissingCard(key);
+
+  // ALL CARDS is sent when all cards have been given to the user
+  if (!unseenCard) {
+    res.send({ id: "ALL CARDS" });
     return;
   }
 
-  if (req.path === "/card_add") {
-    initializeAllCards(); // Needs to run when requests are live so that it doesn't get flushed by the tester
-    const key = "user_id:" + req.query.id;
-    unseenCard = await getMissingCard(key);
-
-    // ALL CARDS is sent when all cards have been given to the user
-    if (!unseenCard) {
-      res.statusCode = 200;
-      res.end(JSON.stringify({ id: "ALL CARDS" }));
-      return;
-    }
-
-    res.statusCode = 200;
-    res.end(unseenCard);
-  }
+  res.send(unseenCard);
 });
 
-const client = require("redis").createClient();
-client.on("error", (err) => console.log("Redis Client Error", err));
-
-client.on("ready", () => {
-  app.listen(port, "0.0.0.0", () => {
-    console.log(`Example app listening at http://0.0.0.0:${port}`);
-  });
+app.get("/ready", async (req, res) => {
+  res.send({ ready: true });
 });
 
 client.connect();
