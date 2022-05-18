1ac95480b868cb4df90afdf34a235c2cae48ef11
Coby Benveniste <41164052+probably-not@users.noreply.github.com>
2022-05-18 21:20:24 +0300

    In Memory Indexes (#1)
    * Leave redis behind and instead use in memory indexes to track the user cards
    
    * Hacks to allow synchronization between multiple processes in the best possible way

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const crypto = require("crypto");
 
 const cardsData = fs.readFileSync("./cards.json");
 const cards = JSON.parse(cardsData);
@@ -7,28 +6,82 @@ const allCards = cards.map((c) => {
   return JSON.stringify(c);
 });
 
-const completedUsers = {};
+const port = +process.argv[2] || 3000;
+const lockFile = "./master.lock";
+let isMaster = true;
+try {
+  fs.writeFileSync(lockFile, `${port}`, { flag: "wx" });
+} catch (err) {
+  console.log("Master Lock Error", err);
+  isMaster = false;
+}
 
-const getUnseenCard = async function (userId) {
-  // Early exit if completed
-  if (completedUsers[userId]) {
-    return undefined;
+let masterPort;
+if (!isMaster) {
+  masterPortStr = fs.readFileSync(lockFile, "utf8");
+  masterPort = parseInt(masterPortStr, 10);
+  fs.unlinkSync(lockFile);
+}
+
+const shutdownHandler = (signal) => {
+  console.log("starting shutdown, got signal " + signal);
+  process.exit(0);
+};
+
+process.on("SIGINT", shutdownHandler);
+process.on("SIGTERM", shutdownHandler);
+
+const net = require("net");
+const forwarder = net.createServer(function (from) {
+  const to = net.createConnection({
+    host: "0.0.0.0",
+    port: masterPort,
+  });
+  from.pipe(to);
+  to.pipe(from);
+});
+
+const http = require("turbo-http");
+server = http.createServer();
+
+if (!isMaster) {
+  console.log(`Forwarding from ${port} to ${masterPort}`);
+  server = forwarder;
+}
+
+const client = require("redis").createClient();
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+client.on("ready", () => {
+  server.listen(port, "0.0.0.0", () => {
+    console.log(`Server listening at http://0.0.0.0:${port}`);
+  });
+});
+
+const userIndexes = {};
+
+const INCR = (userId) => {
+  // Get the next index of the card that the user hasn't seen yet
+  if (!userIndexes[userId]) {
+    // Init
+    userIndexes[userId] = 0;
   }
 
-  // Get the next index of the card that the user hasn't seen yet
-  const idx = await client.INCR(userId);
+  userIndexes[userId]++;
+  return userIndexes[userId];
+};
+
+const getUnseenCard = async (userId) => {
+  const idx = INCR(userId);
   if (idx <= allCards.length) {
     return allCards[idx - 1];
   }
-  completedUsers[userId] = true;
   return undefined;
 };
 
 const cardHandler = async (req, res, userId) => {
-  // const reqid = crypto.randomUUID();
-  // console.time(`${reqid} get unseen card`);
   unseenCard = await getUnseenCard(userId);
-  // console.timeEnd(`${reqid} get unseen card`);
 
   // ALL CARDS is sent when all cards have been given to the user
   if (!unseenCard) {
@@ -40,11 +93,6 @@ const cardHandler = async (req, res, userId) => {
   res.statusCode = 200;
   res.end(unseenCard);
 };
-
-const http = require("turbo-http");
-server = http.createServer();
-const port = +process.argv[2] || 3000;
-const baseUrl = `http://0.0.0.0:${port}`;
 
 const router = async (req, res) => {
   if (req.url.startsWith("/card_add?")) {
@@ -58,13 +106,4 @@ const router = async (req, res) => {
 };
 
 server.on("request", router);
-const client = require("redis").createClient();
-client.on("error", (err) => console.log("Redis Client Error", err));
-
-client.on("ready", () => {
-  server.listen(port, "0.0.0.0", () => {
-    console.log(`Server listening at http://0.0.0.0:${port}`);
-  });
-});
-
 client.connect();
