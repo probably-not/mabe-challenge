0820f63d0ee1b1f9f82c5b707179487621b2a36f
Coby Benveniste <coby@base.la>
2022-05-18 10:21:47 +0300

    TCP socket communication without synced data

--- index.js
+++ index.js
@@ -7,8 +7,46 @@ const allCards = cards.map((c) => {
   return JSON.stringify(c);
 });
 
+const userIndexes = {};
+
+const INCR = (userId) => {
+  // Get the next index of the card that the user hasn't seen yet
+  if (!userIndexes[userId]) {
+    // Init
+    userIndexes[userId] = 0;
+  }
+
+  userIndexes[userId]++;
+  return userIndexes[userId];
+};
+
 const port = +process.argv[2] || 3000;
 let isMaster = true;
+
+const turbo = require("turbo-net");
+const tcpServer = turbo.createServer(function (socket) {
+  socket.read(Buffer.alloc(32 * 1024), function onread(err, buf, _read) {
+    if (err) {
+      console.log("Socket Read Error", err);
+      return;
+    }
+
+    const data = buf.toString();
+    const userId = data.split("\n")[0];
+    userUnseenCardIdx = INCR(userId);
+    const out = Buffer.from(userUnseenCardIdx.toString(), "utf8");
+
+    socket.write(out, out.length, function (err) {
+      if (err) {
+        console.log("Socket Write Error", err);
+        return;
+      }
+      socket.read(buf, onread);
+    });
+  });
+});
+
+let tcpSocket;
 
 const initializeIsMaster = (function () {
   var executed = false;
@@ -19,37 +57,66 @@ const initializeIsMaster = (function () {
 
     const applied = await client.SETNX("is_master_mark", "1");
     isMaster = applied;
-    console.log("am i master?", port, isMaster);
+
+    if (isMaster) {
+      tcpServer.listen(8080, function () {
+        console.log(`TCP Server listening at http://0.0.0.0:8080`);
+      });
+    } else {
+      tcpSocket = turbo.connect(8080);
+      console.log(`TCP Client connected to http://0.0.0.0:8080`);
+    }
+
     executed = true;
   };
 })();
 
-const userIndexes = {};
+const getUnseenCardIdxFromMaster = async (userId) => {
+  let idx;
 
-const getUnseenCard = async function (key) {
-  // Get the next index of the card that the user hasn't seen yet
-  if (!userIndexes[key]) {
-    // Init
-    userIndexes[key] = 0;
+  const readUserIdx = async (parsed) => {
+    idx = parsed;
+  };
+
+  tcpSocket.write(Buffer.from(userId + "\n"));
+  tcpSocket.read(Buffer.alloc(10), (err, buf, _read) => {
+    if (err) {
+      console.log("Client Socket Read Error", err);
+      return;
+    }
+
+    const data = buf.toString();
+    const parsed = parseInt(data, 10);
+    console.log(parsed);
+    readUserIdx(parsed);
+  });
+
+  console.log(idx);
+  return idx;
+};
+
+const getUnseenCard = async (userId) => {
+  if (isMaster) {
+    const idx = INCR(userId);
+    if (idx <= allCards.length) {
+      return allCards[idx - 1];
+    }
+    return undefined;
   }
 
-  // INCR
-  userIndexes[key]++;
+  const idx = await getUnseenCardIdxFromMaster(userId);
+  if (!idx) {
+    return undefined;
+  }
 
-  const idx = userIndexes[key];
   if (idx <= allCards.length) {
     return allCards[idx - 1];
   }
-
   return undefined;
 };
 
 const cardHandler = async (req, res, userId) => {
-  // const reqid = crypto.randomUUID();
-  // console.time(`${reqid} get unseen card`);
-  const key = "user_id:" + userId;
-  unseenCard = await getUnseenCard(key);
-  // console.timeEnd(`${reqid} get unseen card`);
+  unseenCard = await getUnseenCard(userId);
 
   // ALL CARDS is sent when all cards have been given to the user
   if (!unseenCard) {
